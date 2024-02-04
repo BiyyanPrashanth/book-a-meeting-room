b18ba59198564028dea292f1e4e218de6fe5f1dd
PRASHANTHRE <prashanthre@mashreq.com>
2024-02-05 02:34:21 +0400

    code refactor

--- book-a-meeting-room/src/main/java/com/mashreq/bookameetingroom/model/MeetingRoomDTO.java
+++ book-a-meeting-room/src/main/java/com/mashreq/bookameetingroom/model/MeetingRoomDTO.java
@@ -1,11 +1,13 @@
 package com.mashreq.bookameetingroom.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class MeetingRoomDTO {
     private Long id;
     private String name;
@@ -13,4 +15,10 @@ public class MeetingRoomDTO {
     private String MeetingRoomStatus;
     private List<BookingDTO> bookings;
 
+    public MeetingRoomDTO(Long id, String name, Integer capacity, List<BookingDTO> bookings) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.bookings = bookings;
+    }
 }
--- book-a-meeting-room/src/main/java/com/mashreq/bookameetingroom/service/impl/MeetingRoomServiceImpl.java
+++ book-a-meeting-room/src/main/java/com/mashreq/bookameetingroom/service/impl/MeetingRoomServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class MeetingRoomServiceImpl implements MeetingRoomService {
@@ -40,26 +41,22 @@ public class MeetingRoomServiceImpl implements MeetingRoomService {
         List<MeetingRoomDTO> meetingRoomDTOS = Mapper.convertToMeetingDTO(allMeetingRooms);
         while (startTime.isBefore(endTime)) {
             LocalDateTime slotEndTime = startTime.plusMinutes(appProp.getSlotTime());
-            List<MeetingRoomDTO> availableMeetingRooms = new ArrayList<>();
             LocalDateTime finalStartTime = startTime;
-            meetingRoomDTOS.stream().forEach(meetingRoom ->{
-               String slotAvailable= isAvailable(finalStartTime, slotEndTime, meetingRoom.getBookings());
+            List<MeetingRoomDTO> availableMeetingRooms = meetingRoomDTOS.stream().map(meetingRoom -> {
+                String slotAvailable = isAvailable(finalStartTime, slotEndTime, meetingRoom.getBookings());
+                MeetingRoomDTO updatedMeetingRoom = new MeetingRoomDTO(meetingRoom.getId(), meetingRoom.getName(), meetingRoom.getCapacity(), meetingRoom.getBookings());
                 if (slotAvailable.equalsIgnoreCase(ApplicationConstants.AVAILABLE)) {
-                    meetingRoom.setMeetingRoomStatus(ApplicationConstants.AVAILABLE);
-                    availableMeetingRooms.add(meetingRoom);
-                } else if (slotAvailable.equalsIgnoreCase(ApplicationConstants.BOOKED))  {
-                    meetingRoom.setMeetingRoomStatus(ApplicationConstants.BOOKED);
-                    availableMeetingRooms.add(meetingRoom);
+                    updatedMeetingRoom.setMeetingRoomStatus(ApplicationConstants.AVAILABLE);
+                } else if (slotAvailable.equalsIgnoreCase(ApplicationConstants.BOOKED)) {
+                    updatedMeetingRoom.setMeetingRoomStatus(ApplicationConstants.BOOKED);
                 } else {
-                    meetingRoom.setMeetingRoomStatus(ApplicationConstants.MAINTENANCE);
-                    availableMeetingRooms.add(meetingRoom);
+                    updatedMeetingRoom.setMeetingRoomStatus(ApplicationConstants.MAINTENANCE);
                 }
-
-            });
+                return updatedMeetingRoom;
+            }).collect(Collectors.toList());
             availableTimeSlots.add(new AvailableTimeSlot(startTime, slotEndTime, availableMeetingRooms));
-            startTime = slotEndTime;
+            startTime = slotEndTime.plusMinutes(1);
         }
-
         return availableTimeSlots;
     }
 
@@ -72,14 +69,14 @@ public class MeetingRoomServiceImpl implements MeetingRoomService {
         availableRooms.sort(Comparator.comparingInt(MeetingRoom::getCapacity).reversed());
         if (!availableRoomsDTO.isEmpty()) {
             for (MeetingRoomDTO meetingRoom : availableRoomsDTO) {
-                String slotAvailable=isAvailable(startTime, endTime, meetingRoom.getBookings());
+                String slotAvailable = isAvailable(startTime, endTime, meetingRoom.getBookings());
                 if (slotAvailable.equalsIgnoreCase(ApplicationConstants.AVAILABLE) && meetingRoom.getCapacity() >= numberOfPeople) {
                     BookingDetails newBooking = new BookingDetails(startTime, endTime, numberOfPeople, new MeetingRoom(meetingRoom.getId()));
                     bookRoomRepository.save(newBooking);
                     return new BookingResponse("Meeting room booked successfully: " + meetingRoom.getName(), HttpStatus.OK);
                 } else if (slotAvailable.equalsIgnoreCase(ApplicationConstants.BOOKED)) {
                     throw new BookingException("Meeting room is already booked during the requested time range");
-                } else{
+                } else {
                     throw new BookingException("meeting room is under maintenance  during the requested time range");
                 }
             }
@@ -92,36 +89,39 @@ public class MeetingRoomServiceImpl implements MeetingRoomService {
 
     // Method to check if the room is available for a given time range
     private String isAvailable(LocalDateTime startTime, LocalDateTime endTime, List<BookingDTO> bookings) {
-
-            Boolean isRoomUnderMaintenance = checkRoomMaintenance(startTime, endTime);
+        boolean isBooked = false;
+        Boolean isRoomUnderMaintenance = checkRoomMaintenance(startTime, endTime);
         if (!isRoomUnderMaintenance) {
             if (Objects.nonNull(bookings)) {
-                for (BookingDTO booking : bookings) {
-                    if (startTime.isBefore(booking.getEndTime()) && endTime.isAfter(booking.getStartTime())) {
-                        return ApplicationConstants.BOOKED;
-                    }
-                }
+                isBooked = bookings.stream()
+                        .anyMatch(booking -> isTimeSlotAvailable(booking, startTime, endTime));
             }
         } else {
             return ApplicationConstants.MAINTENANCE;
         }
-        return ApplicationConstants.AVAILABLE;
+        return isBooked ? ApplicationConstants.BOOKED : ApplicationConstants.AVAILABLE;
     }
 
     private Boolean checkRoomMaintenance(LocalDateTime startTime, LocalDateTime endTime) {
         List<String> maintenanceSlots = appProp.getMaintenanceSlot();
-        for (String timeslot : maintenanceSlots) {
-            String[] splitTimeSlot = timeslot.split(ApplicationConstants.REGEX);
-            LocalDateTime maintenanceStart = dateForHourMinutes(splitTimeSlot[0]);
-            LocalDateTime maintenanceEnd = dateForHourMinutes(splitTimeSlot[1]);
-            if (startTime.isBefore(maintenanceEnd) && endTime.isAfter(maintenanceStart)) {
-                return true;
-            }
-        }
-        return false;
+        boolean isUnderMaintenance = maintenanceSlots.stream()
+                .anyMatch(timeslot -> isWithinTimeSlot(timeslot, startTime, endTime));
+
+        return isUnderMaintenance ? Boolean.TRUE : Boolean.FALSE;
     }
 
-    private LocalDateTime dateForHourMinutes(String hourMinu) {
+    private static boolean isWithinTimeSlot(String timeSlot, LocalDateTime startTime, LocalDateTime endTime) {
+        String[] splitTimeSlot = timeSlot.split(ApplicationConstants.REGEX);
+        LocalDateTime maintenanceStart = dateForHourMinutes(splitTimeSlot[0]);
+        LocalDateTime maintenanceEnd = dateForHourMinutes(splitTimeSlot[1]);
+        return (startTime.isBefore(maintenanceEnd) && endTime.isAfter(maintenanceStart));
+    }
+
+    private static boolean isTimeSlotAvailable(BookingDTO booking, LocalDateTime startTime, LocalDateTime endTime) {
+        return (startTime.isBefore(booking.getEndTime()) && endTime.isAfter(booking.getStartTime()));
+    }
+
+    private static LocalDateTime dateForHourMinutes(String hourMinu) {
         LocalDateTime startDateTime = LocalDateTime.now()
                 .withHour(Integer.parseInt(hourMinu.split(":")[0]))
                 .withMinute(Integer.parseInt(hourMinu.split(":")[1]));
